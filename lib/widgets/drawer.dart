e27e4b7412da9c048836db6e4a85c362a2bcf676
pramilaniroshan <pramilaniroshan@gmail.com>
2022-06-23 20:02:33 +0530

    Custome Drawer added

---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,318 @@
+import 'package:emedassistantmobile/screens/auth/home/home_screen.dart';
+import 'package:emedassistantmobile/screens/calendar/calendar_screen.dart';
+import 'package:emedassistantmobile/screens/scan_qr/scan_qr_screen.dart';
+import 'package:emedassistantmobile/screens/settings/setting_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+import 'package:emedassistantmobile/config/app_colors.dart';
+import 'package:emedassistantmobile/config/app_images.dart';
+
+
+class DoctorDrawer extends StatelessWidget {
+
+  late SharedPreferences prefs;
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Drawer(
+        backgroundColor: AppColors.white,
+        elevation: 0.0,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(height: 40.0),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: IconButton(
+                onPressed: (){
+                  Get.back();
+                },
+                icon: SvgPicture.asset(
+                  AppImages.closeIcon,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            ListTile(
+              onTap: (){},
+              leading: Padding(
+                padding: const EdgeInsets.only(top: 6.0, left: 12.0),
+                child: SvgPicture.asset(
+                  AppImages.calendarIconMenu,
+                  height: 13.0,
+                  width: 13.0,
+                  fit: BoxFit.scaleDown,
+                  //color: AppColors.secondary,
+                ),
+              ),
+              title: const Align(
+                alignment: Alignment(-1.4, 0),
+                child: Text('Appointments',
+                  style: TextStyle(
+                    fontSize: 21.0,
+                    color: AppColors.black,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              onTap: (){
+                Get.to(CalendarScreen());
+              },
+              leading: Padding(
+                padding: const EdgeInsets.only(top: 6.0, left: 12.0),
+                child: SvgPicture.asset(
+                  AppImages.calendarIconMenu,
+                  height: 13.0,
+                  width: 13.0,
+                  fit: BoxFit.scaleDown,
+                  color: AppColors.secondary,
+                ),
+              ),
+              title: const Align(
+                alignment: Alignment(-1.3, 0),
+                child: Text('Planner',
+                  style: TextStyle(
+                    fontSize: 21.0,
+                    color: AppColors.black,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              onTap: (){
+                Get.to(ScanQrScreen());
+              },
+              leading: Padding(
+                padding: const EdgeInsets.only(top: 6.0, left: 12.0),
+                child: SvgPicture.asset(
+                  AppImages.termsIcon,
+                  height: 13.0,
+                  width: 13.0,
+                  fit: BoxFit.scaleDown,
+                  color: AppColors.secondary,
+                ),
+              ),
+              title: const Align(
+                alignment: Alignment(-1.3, 0),
+                child: Text('My QRcode',
+                  style: TextStyle(
+                    fontSize: 21.0,
+                    color: AppColors.black,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              onTap: (){},
+              leading: Padding(
+                padding: const EdgeInsets.only(top: 6.0, left: 12.0),
+                child: SvgPicture.asset(
+                  AppImages.linkIcon,
+                  height: 13.0,
+                  width: 13.0,
+                  fit: BoxFit.scaleDown,
+                  color: AppColors.secondary,
+                ),
+              ),
+              title: const Align(
+                alignment: Alignment(-1.3, 0),
+                child: Text('Reports',
+                  style: TextStyle(
+                    fontSize: 21.0,
+                    color: AppColors.black,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              onTap: (){},
+              leading: Padding(
+                padding: const EdgeInsets.only(top: 6.0, left: 12.0),
+                child: SvgPicture.asset(
+                  AppImages.linkIcon,
+                  height: 13.0,
+                  width: 13.0,
+                  fit: BoxFit.scaleDown,
+                  color: AppColors.secondary,
+                ),
+              ),
+              title: const Align(
+                alignment: Alignment(-1.3, 0),
+                child: Text('Assistants',
+                  style: TextStyle(
+                    fontSize: 21.0,
+                    color: AppColors.black,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              onTap: (){},
+              leading: Padding(
+                padding: const EdgeInsets.only(top: 6.0, left: 12.0),
+                child: SvgPicture.asset(
+                  AppImages.linkIcon,
+                  height: 13.0,
+                  width: 13.0,
+                  fit: BoxFit.scaleDown,
+                  color: AppColors.secondary,
+                ),
+              ),
+              title: const Align(
+                alignment: Alignment(-1.3, 0),
+                child: Text('Doctors',
+                  style: TextStyle(
+                    fontSize: 21.0,
+                    color: AppColors.black,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              onTap: (){},
+              leading: Padding(
+                padding: const EdgeInsets.only(top: 6.0, left: 12.0),
+                child: SvgPicture.asset(
+                  AppImages.linkIcon,
+                  height: 13.0,
+                  width: 13.0,
+                  fit: BoxFit.scaleDown,
+                  color: AppColors.secondary,
+                ),
+              ),
+              title: const Align(
+                alignment: Alignment(-1.3, 0),
+                child: Text('Locations',
+                  style: TextStyle(
+                    fontSize: 21.0,
+                    color: AppColors.black,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              onTap: (){},
+              leading: Padding(
+                padding: const EdgeInsets.only(top: 6.0, left: 12.0),
+                child: SvgPicture.asset(
+                  AppImages.linkIcon,
+                  height: 13.0,
+                  width: 13.0,
+                  fit: BoxFit.scaleDown,
+                  color: AppColors.secondary,
+                ),
+              ),
+              title: const Align(
+                alignment: Alignment(-1.3, 0),
+                child: Text('Messages',
+                  style: TextStyle(
+                    fontSize: 21.0,
+                    color: AppColors.black,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              onTap: (){
+                 Get.to(SettingsScreen());
+              },
+              leading: Padding(
+                padding: const EdgeInsets.only(top: 6.0, left: 12.0),
+                child: SvgPicture.asset(
+                  AppImages.linkIcon,
+                  height: 13.0,
+                  width: 13.0,
+                  fit: BoxFit.scaleDown,
+                  color: AppColors.secondary,
+                ),
+              ),
+              title: const Align(
+                alignment: Alignment(-1.3, 0),
+                child: Text('Settings',
+                  style: TextStyle(
+                    fontSize: 21.0,
+                    color: AppColors.black,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              onTap: () async{
+          prefs = await SharedPreferences.getInstance();
+          prefs.clear();
+          ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Logged out!'),
+          backgroundColor: AppColors.lightBlue,
+          ),
+      );
+          Get.to(const HomeScreen());
+        },
+              leading: Padding(
+                padding: const EdgeInsets.only(top: 6.0, left: 12.0),
+                child: SvgPicture.asset(
+                  AppImages.linkIcon,
+                  height: 13.0,
+                  width: 13.0,
+                  fit: BoxFit.scaleDown,
+                  color: AppColors.secondary,
+                ),
+              ),
+              title: const Align(
+                alignment: Alignment(-1.3, 0),
+                child: Text('Sign off',
+                  style: TextStyle(
+                    fontSize: 21.0,
+                    color: AppColors.black,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: height * 0.06),
+            ListTile(
+              onTap: (){},
+              leading: Padding(
+                padding: const EdgeInsets.only(top: 6.0, left: 12.0),
+                child: SvgPicture.asset(
+                  AppImages.globeIcon,
+                  height: 13.0,
+                  width: 13.0,
+                  fit: BoxFit.scaleDown,
+                  color: AppColors.secondary,
+                ),
+              ),
+              title: const Align(
+                alignment: Alignment(-1.1, 0),
+                child: Text('English',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: AppColors.black,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              trailing: const Icon(Icons.keyboard_arrow_down_outlined, color: AppColors.black),
+            ),
+          ],
+        ),
+      );
+  }
+}
