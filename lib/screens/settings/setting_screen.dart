88e86938b28abb6f61acc2469566e015fe91098f
pramilaniroshan <pramilaniroshan@gmail.com>
2022-06-23 20:03:51 +0530

    Shared Pref init

--- lib/screens/settings/setting_screen.dart
+++ lib/screens/settings/setting_screen.dart
@@ -1,9 +1,12 @@
 import 'package:emedassistantmobile/screens/settings/components/locations_screen.dart';
 import 'package:emedassistantmobile/screens/settings/components/s_profile_screen.dart';
 import 'package:emedassistantmobile/screens/settings/components/specialities_screen.dart';
+import 'package:emedassistantmobile/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:dio/dio.dart';
 
 import 'package:emedassistantmobile/config/app_colors.dart';
 import 'package:emedassistantmobile/config/app_images.dart';
@@ -23,11 +26,18 @@ class _SettingsScreenState extends State<SettingsScreen> with SingleTickerProvid
 
 
   TabController? tabController;
+  late SharedPreferences prefs;
+
+  void GetProfile () async{
+    prefs = await SharedPreferences.getInstance();
+    print(prefs.getString('name'));
+  }
 
   @override
   void initState() {
     tabController = TabController(length: 5, vsync: this);
     super.initState();
+    GetProfile();
   }
 
   @override
@@ -69,146 +79,7 @@ class _SettingsScreenState extends State<SettingsScreen> with SingleTickerProvid
           menuButton(),
         ],
       ),
-      endDrawer: Drawer(
-        backgroundColor: AppColors.white,
-        elevation: 0.0,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const SizedBox(height: 40.0),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: IconButton(
-                onPressed: (){
-                  Get.back();
-                },
-                icon: SvgPicture.asset(
-                  AppImages.closeIcon,
-                ),
-              ),
-            ),
-            const SizedBox(height: 20.0),
-            ListTile(
-              onTap: (){},
-              leading: Padding(
-                padding: const EdgeInsets.only(top: 6.0, left: 12.0),
-                child: SvgPicture.asset(
-                  AppImages.supportIcon,
-                  height: 13.0,
-                  width: 13.0,
-                  fit: BoxFit.scaleDown,
-                  color: AppColors.secondary,
-                ),
-              ),
-              title: const Align(
-                alignment: Alignment(-1.3, 0),
-                child: Text('Support',
-                  style: TextStyle(
-                    fontSize: 21.0,
-                    color: AppColors.black,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ),
-            ),
-            ListTile(
-              onTap: (){},
-              leading: Padding(
-                padding: const EdgeInsets.only(top: 6.0, left: 12.0),
-                child: SvgPicture.asset(
-                  AppImages.contactIcon,
-                  height: 13.0,
-                  width: 13.0,
-                  fit: BoxFit.scaleDown,
-                  color: AppColors.secondary,
-                ),
-              ),
-              title: const Align(
-                alignment: Alignment(-1.3, 0),
-                child: Text('Contact',
-                  style: TextStyle(
-                    fontSize: 21.0,
-                    color: AppColors.black,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ),
-            ),
-            ListTile(
-              onTap: (){},
-              leading: Padding(
-                padding: const EdgeInsets.only(top: 6.0, left: 12.0),
-                child: SvgPicture.asset(
-                  AppImages.termsIcon,
-                  height: 13.0,
-                  width: 13.0,
-                  fit: BoxFit.scaleDown,
-                  color: AppColors.secondary,
-                ),
-              ),
-              title: const Align(
-                alignment: Alignment(-1.3, 0),
-                child: Text('Terms',
-                  style: TextStyle(
-                    fontSize: 21.0,
-                    color: AppColors.black,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ),
-            ),
-            ListTile(
-              onTap: (){},
-              leading: Padding(
-                padding: const EdgeInsets.only(top: 6.0, left: 12.0),
-                child: SvgPicture.asset(
-                  AppImages.linkIcon,
-                  height: 13.0,
-                  width: 13.0,
-                  fit: BoxFit.scaleDown,
-                  color: AppColors.secondary,
-                ),
-              ),
-              title: const Align(
-                alignment: Alignment(-1.3, 0),
-                child: Text('eMed.com',
-                  style: TextStyle(
-                    fontSize: 21.0,
-                    color: AppColors.black,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: height * 0.06),
-            ListTile(
-              onTap: (){},
-              leading: Padding(
-                padding: const EdgeInsets.only(top: 6.0, left: 12.0),
-                child: SvgPicture.asset(
-                  AppImages.globeIcon,
-                  height: 13.0,
-                  width: 13.0,
-                  fit: BoxFit.scaleDown,
-                  color: AppColors.secondary,
-                ),
-              ),
-              title: const Align(
-                alignment: Alignment(-1.1, 0),
-                child: Text('English',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    color: AppColors.black,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ),
-              trailing: const Icon(Icons.keyboard_arrow_down_outlined, color: AppColors.black),
-            ),
-          ],
-        ),
-      ),
+      endDrawer: DoctorDrawer(),
 
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
