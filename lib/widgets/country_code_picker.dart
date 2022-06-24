820bfa639b00f45df80532bc6c13e5e0fc18b55a
pramilaniroshan <pramilaniroshan@gmail.com>
2022-06-24 15:31:21 +0530

    Country picker added

---
+++ lib/widgets/country_code_picker.dart
@@ -0,0 +1,29 @@
+// import 'package:country_code_picker/country_code_picker.dart';
+// import 'package:country_code_picker/country_localizations.dart';
+// import 'package:flutter/material.dart';
+
+// class CountryCodePicker extends StatefulWidget {
+//   const CountryCodePicker({Key? key,
+// }) : super(key: key);
+
+//   @override
+//   State<CountryCodePicker> createState() => _CountryCodePickerState();
+// }
+
+// class _CountryCodePickerState extends State<CountryCodePicker> {
+// @override
+//  Widget build(BuildContext context) => const Scaffold(
+//      body: Center(
+//        child: CountryCodePicker(
+//          initialSelection: 'IT',
+//          // optional. Shows only country name and flag
+//          showCountryOnly: false,
+//          // optional. Shows only country name and flag when popup is closed.
+//          showOnlyCountryWhenClosed: false,
+//          // optional. aligns the flag and the Text left
+//          alignLeft: false,
+//        ),
+      
+//      ),
+//  );
+// }
