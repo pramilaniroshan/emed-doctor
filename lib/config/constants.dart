bb97ea47fbe606bf29ea5260b89bdce3cfaa37cc
pramilaniroshan <pramilaniroshan@gmail.com>
2023-01-27 08:52:43 +0530

    Base URL changed

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,6 +1,6 @@
 class Constants {
   //String baseUrl = 'http://localhost:5000/api/v1';
   //String baseUrl = 'http://10.0.2.2:5000/api/v1';
-  String baseUrl = 'https://emedapi.azurewebsites.net/api/v1';
+  String baseUrl = 'http://194.163.185.232/api/v1';
   String getBaseUrl() => baseUrl;
 }
